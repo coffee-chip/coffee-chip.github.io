10235f2ed906ae19b7fa40e44a12509f261595b2
coffee-chip <jlsieving@gmail.com>
2026-08-02 00:06:15 -0400

    Add colors and icon keys to type metadata

--- pokemon/js/data/types.js
+++ pokemon/js/data/types.js
@@ -3,8 +3,21 @@ export const TYPES = [
   'flying','psychic','bug','rock','ghost','dragon','dark','steel','fairy'
 ];
 
+const TYPE_COLORS = {
+  normal: '#929DA3', fire: '#FF9D55', water: '#5090D6', electric: '#F4D23C',
+  grass: '#63BC5A', ice: '#73CEC0', fighting: '#CE416B', poison: '#AA6BC8',
+  ground: '#D97845', flying: '#8FA9DE', psychic: '#FA7179', bug: '#91C12F',
+  rock: '#C5B78C', ghost: '#5269AD', dragon: '#0B6DC3', dark: '#5A5465',
+  steel: '#5A8EA2', fairy: '#EC8FE6'
+};
+
 export const TYPE_META = Object.fromEntries(
-  TYPES.map(id => [id, { id, label: id[0].toUpperCase() + id.slice(1) }])
+  TYPES.map(id => [id, {
+    id,
+    label: id[0].toUpperCase() + id.slice(1),
+    color: TYPE_COLORS[id],
+    icon: id
+  }])
 );
 
 // One source of truth: attacking type -> defending type -> multiplier.
