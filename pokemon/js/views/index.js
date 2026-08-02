ddc5439d254b48d22cfaad653e24202ca00f29e9
coffee-chip <jlsieving@gmail.com>
2026-08-02 13:57:08 -0400

    Use modular service worker diagnostics view

--- pokemon/js/views/index.js
+++ pokemon/js/views/index.js
@@ -2,10 +2,12 @@ import { VIEWS as BASE_VIEWS } from '../views.js';
 import { renderQuiz } from './quiz.js';
 import { renderStudy } from './study.js';
 import { renderSettings } from './settings.js';
+import { renderDebug } from './debug.js';
 
 export const VIEWS = {
   ...BASE_VIEWS,
   quiz: renderQuiz,
   study: renderStudy,
-  settings: renderSettings
+  settings: renderSettings,
+  debug: renderDebug
 };
