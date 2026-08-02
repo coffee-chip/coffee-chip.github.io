54721fe1e2310de002b0a63e17b05f3448e363d0
coffee-chip <jlsieving@gmail.com>
2026-08-01 23:56:15 -0400

    Use modular settings view

--- pokemon/js/views/index.js
+++ pokemon/js/views/index.js
@@ -1,7 +1,9 @@
 import { VIEWS as BASE_VIEWS } from '../views.js';
 import { renderStudy } from './study.js';
+import { renderSettings } from './settings.js';
 
 export const VIEWS = {
   ...BASE_VIEWS,
-  study: renderStudy
+  study: renderStudy,
+  settings: renderSettings
 };
