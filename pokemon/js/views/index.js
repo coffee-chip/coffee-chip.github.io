5941cbd107dd9cd4a5b9a49e92e5723975a0ba84
coffee-chip <jlsieving@gmail.com>
2026-08-02 16:20:09 -0400

    Use dedicated relationship progress view

--- pokemon/js/views/index.js
+++ pokemon/js/views/index.js
@@ -1,6 +1,7 @@
 import { VIEWS as BASE_VIEWS } from '../views.js';
 import { renderQuiz } from './quiz.js';
 import { renderStudy } from './study.js';
+import { renderProgress } from './progress.js';
 import { renderSettings } from './settings.js';
 import { renderDebug } from './debug.js';
 
@@ -8,6 +9,7 @@ export const VIEWS = {
   ...BASE_VIEWS,
   quiz: renderQuiz,
   study: renderStudy,
+  progress: renderProgress,
   settings: renderSettings,
   debug: renderDebug
 };
