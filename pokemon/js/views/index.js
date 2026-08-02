7cbae615ff159bf017f3e8ae82431003a7a056a6
coffee-chip <jlsieving@gmail.com>
2026-08-02 00:08:31 -0400

    Use modular quiz view with type badges

--- pokemon/js/views/index.js
+++ pokemon/js/views/index.js
@@ -1,9 +1,11 @@
 import { VIEWS as BASE_VIEWS } from '../views.js';
+import { renderQuiz } from './quiz.js';
 import { renderStudy } from './study.js';
 import { renderSettings } from './settings.js';
 
 export const VIEWS = {
   ...BASE_VIEWS,
+  quiz: renderQuiz,
   study: renderStudy,
   settings: renderSettings
 };
