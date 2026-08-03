43d99527ddf2533e5b3ef4bf4352d8297f64a917
coffee-chip <jlsieving@gmail.com>
2026-08-03 14:57:43 -0400

    Bump app cache for nonblocking Pokemon autocomplete

--- pokemon/service-worker.js
+++ pokemon/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.03.9';
+const APP_VERSION = '2026.08.03.10';
 const CACHE_PREFIX = 'pokemon-type-trainer-shell-';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const CORE_ASSETS = [
