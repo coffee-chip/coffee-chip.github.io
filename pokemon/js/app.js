af178a3c2351fa585961746b4ef18188bcbc90b1
coffee-chip <jlsieving@gmail.com>
2026-08-04 10:28:18 -0400

    Wire recognition quiz setup controls

--- pokemon/js/app.js
+++ pokemon/js/app.js
@@ -9,6 +9,7 @@ import { initializePokemonAutocomplete } from './components/pokemonAutocomplete.
 import { enhancePokemonLookupResult } from './components/pokemonMatchupEnhancer.js';
 import { enhancePokemonEvolutionControls } from './components/pokemonEvolutionControls.js';
 import { enhanceStudyTabs } from './components/studyTabs.js';
+import { enhanceQuizRecognitionSettings } from './components/quizRecognitionSettings.js';
 import { getPokemonNameIndex } from './data/pokemonRepository.js';
 import { applyTheme, watchSystemTheme } from './theme.js';
 import { renderDeveloperOverlay } from './developerOverlay.js';
@@ -51,6 +52,7 @@ function render() {
   enhanceStudyTabs(viewRoot);
   enhancePokemonEvolutionControls(viewRoot);
   enhancePokemonLookupResult(viewRoot);
+  enhanceQuizRecognitionSettings(viewRoot);
   for (const link of navLinks) {
     if (link.dataset.route === state.route) link.setAttribute('aria-current', 'page');
     else link.removeAttribute('aria-current');
