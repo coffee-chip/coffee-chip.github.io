4c37ee0385525d3dadfecfa2ee62c42b278f0060
coffee-chip <jlsieving@gmail.com>
2026-08-04 06:13:22 -0400

    Deploy async Pokémon recognition quiz

--- pokemon/service-worker.js
+++ pokemon/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.04.5';
+const APP_VERSION = '2026.08.04.6';
 const CACHE_PREFIX = 'pokemon-type-trainer-shell-';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const CORE_ASSETS = [
@@ -12,6 +12,7 @@ const CORE_ASSETS = [
   './css/mnemonics.css',
   './css/pokemon-lookup.css',
   './css/study-navigation.css',
+  './css/quiz-pokemon.css',
   './js/app.js',
   './js/serviceWorker.js',
   './js/developerOverlay.js',
