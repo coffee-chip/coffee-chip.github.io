65d3ba805c1da9241efe8d31383593b3504bdb69
coffee-chip <jlsieving@gmail.com>
2026-08-03 21:20:17 -0400

    Bump developer overlay build version

--- pokemon/js/developerOverlay.js
+++ pokemon/js/developerOverlay.js
@@ -1,7 +1,7 @@
 import { state } from './state.js';
 import { serviceWorkerState, subscribeServiceWorker } from './serviceWorker.js';
 
-const APP_BUILD = '2026.08.03.18';
+const APP_BUILD = '2026.08.03.19';
 let overlay = null;
 let unsubscribe = null;
 
