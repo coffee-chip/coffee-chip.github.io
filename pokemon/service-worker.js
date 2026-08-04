0e9d4e8c3b7c946b1a6172d07de9dca0573fbf50
coffee-chip <jlsieving@gmail.com>
2026-08-04 04:59:46 -0400

    Bump app version for Pokemon card layout

--- pokemon/service-worker.js
+++ pokemon/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.03.19';
+const APP_VERSION = '2026.08.04.1';
 const CACHE_PREFIX = 'pokemon-type-trainer-shell-';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const CORE_ASSETS = [
