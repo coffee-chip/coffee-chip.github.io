6cf09a9da1905014de961e58691347c764b7c05a
coffee-chip <jlsieving@gmail.com>
2026-08-04 12:56:43 -0400

    Bump app version for expanded matchup presets

--- pokemon/service-worker.js
+++ pokemon/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.04.12';
+const APP_VERSION = '2026.08.04.13';
 const CACHE_PREFIX = 'pokemon-type-trainer-shell-';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const CORE_ASSETS = [
