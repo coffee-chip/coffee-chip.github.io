aa0e1371effe2dcc35ffc354c2918f5c009b5ed6
coffee-chip <jlsieving@gmail.com>
2026-08-05 13:50:20 -0400

    Remove legacy view module from route registry

--- pokemon/js/views/index.js
+++ pokemon/js/views/index.js
@@ -1,15 +1,13 @@
-import { VIEWS as BASE_VIEWS } from '../views.js';
 import { renderQuiz } from './quiz.js';
 import { renderStudy } from './study.js';
 import { renderProgress } from './progress.js';
 import { renderSettings } from './settings.js';
 import { renderDebug } from './debug.js';
 
-export const VIEWS = {
-  ...BASE_VIEWS,
+export const VIEWS = Object.freeze({
   quiz: renderQuiz,
   study: renderStudy,
   progress: renderProgress,
   settings: renderSettings,
   debug: renderDebug
-};
+});
