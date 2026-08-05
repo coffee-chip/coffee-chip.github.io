be79f1c8cb2618cbbcc48665f2023e6cca267b99
coffee-chip <jlsieving@gmail.com>
2026-08-05 16:18:57 -0400

    Update developer overlay build version

--- pokemon/js/developerOverlay.js
+++ pokemon/js/developerOverlay.js
@@ -1,7 +1,7 @@
 import { state } from './state.js';
 import { serviceWorkerState, subscribeServiceWorker } from './serviceWorker.js';
 
-const APP_BUILD = '2026.08.05.7';
+const APP_BUILD = '2026.08.05.8';
 let overlay = null;
 let unsubscribe = null;
 
