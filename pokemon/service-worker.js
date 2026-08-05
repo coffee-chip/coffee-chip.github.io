637c87c5386f0a0bb59714aa7e1b8c395122b2df
coffee-chip <jlsieving@gmail.com>
2026-08-05 15:12:48 -0400

    Bump app version for open-ended quizzes

--- pokemon/service-worker.js
+++ pokemon/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.05.2';
+const APP_VERSION = '2026.08.05.3';
 const CACHE_PREFIX = 'pokemon-type-trainer-shell-';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const CORE_ASSETS = [
