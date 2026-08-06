354fa493e27b774b1d82c8f798e5430f5d586893
coffee-chip <jlsieving@gmail.com>
2026-08-06 01:18:30 -0400

    Bump app version to 2026.08.06.2

--- pokemon/service-worker.js
+++ pokemon/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.06.1';
+const APP_VERSION = '2026.08.06.2';
 const CACHE_PREFIX = 'pokemon-type-trainer-shell-';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const CORE_ASSETS = [
