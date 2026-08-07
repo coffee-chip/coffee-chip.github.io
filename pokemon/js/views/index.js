c3d1418fd3a6fefea8314c98a258b2b9e05ca92c
coffee-chip <jlsieving@gmail.com>
2026-08-07 01:45:09 -0400

    Register team detail view

--- pokemon/js/views/index.js
+++ pokemon/js/views/index.js
@@ -1,6 +1,7 @@
 import { renderQuiz } from './quiz.js';
 import { renderStudy } from './study.js';
 import { renderTeams } from './teams.js';
+import { renderTeamDetail } from './teamDetail.js';
 import { renderProgress } from './progress.js';
 import { renderSettings } from './settings.js';
 import { renderDebug } from './debug.js';
@@ -9,6 +10,7 @@ export const VIEWS = Object.freeze({
   quiz: renderQuiz,
   study: renderStudy,
   teams: renderTeams,
+  team: renderTeamDetail,
   progress: renderProgress,
   settings: renderSettings,
   debug: renderDebug
