7e79bc6f559261feb06e5e880e07181c4cb4c7c8
coffee-chip <jlsieving@gmail.com>
2026-08-07 02:38:23 -0400

    Bump app version for opponent teams

--- pokemon/service-worker.js
+++ pokemon/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.07.03';
+const APP_VERSION = '2026.08.07.04';
 const CACHE_PREFIX = 'pokemon-type-trainer-shell-';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const CORE_ASSETS = [
