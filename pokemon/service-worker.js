9d4c7e0687791aa40c0e257cd73d72d0fddddcb7
coffee-chip <jlsieving@gmail.com>
2026-08-07 22:18:47 -0400

    Bump app version for battle choice fix

--- pokemon/service-worker.js
+++ pokemon/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.07.23';
+const APP_VERSION = '2026.08.07.24';
 const CACHE_PREFIX = 'pokemon-type-trainer-shell-';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const CORE_ASSETS = [
