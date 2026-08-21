2da87693f0fd055db3bda69b3bff21094d92cc2e
coffee-chip <jlsieving@gmail.com>
2026-08-20 22:09:19 -0400

    Add generation-aware type rules

--- pokemon/js/data/types.js
+++ pokemon/js/data/types.js
@@ -1,3 +1,5 @@
+import { getGameVersionGroup } from './gameVersions.js';
+
 export const TYPES = [
   'normal','fire','water','electric','grass','ice','fighting','poison','ground',
   'flying','psychic','bug','rock','ghost','dragon','dark','steel','fairy'
@@ -12,7 +14,7 @@ export const TYPE_META = Object.fromEntries(
   }])
 );
 
-// One source of truth: attacking type -> defending type -> multiplier.
+// One source of truth for the current chart; earlier generation charts derive from it below.
 export const OFFENSIVE_CHART = {
   normal:{rock:.5,ghost:0,steel:.5},
   fire:{fire:.5,water:.5,grass:2,ice:2,bug:2,rock:.5,dragon:.5,steel:2},
@@ -33,3 +35,45 @@ export const OFFENSIVE_CHART = {
   steel:{fire:.5,water:.5,electric:.5,ice:2,rock:2,steel:.5,fairy:2},
   fairy:{fire:.5,fighting:2,poison:.5,dragon:2,dark:2,steel:.5}
 };
+
+function copyChart() {
+  return Object.fromEntries(TYPES.map(type => [type, { ...OFFENSIVE_CHART[type] }]));
+}
+
+function removeTypes(chart, unavailableTypes) {
+  for (const type of unavailableTypes) delete chart[type];
+  for (const relations of Object.values(chart)) {
+    for (const type of unavailableTypes) delete relations[type];
+  }
+}
+
+export function getTypesForGeneration(generationNumber) {
+  if (generationNumber <= 1) return TYPES.filter(type => !['dark', 'steel', 'fairy'].includes(type));
+  if (generationNumber <= 5) return TYPES.filter(type => type !== 'fairy');
+  return [...TYPES];
+}
+
+export function getTypesForVersionGroup(versionGroup) {
+  return getTypesForGeneration(getGameVersionGroup(versionGroup).generationNumber);
+}
+
+export function getOffensiveChartForGeneration(generationNumber) {
+  const chart = copyChart();
+  if (generationNumber <= 5) {
+    removeTypes(chart, ['fairy']);
+    chart.dark.steel = .5;
+    chart.ghost.steel = .5;
+  }
+  if (generationNumber <= 1) {
+    removeTypes(chart, ['dark', 'steel']);
+    chart.bug.poison = 2;
+    chart.poison.bug = 2;
+    chart.ghost.psychic = 0;
+    delete chart.ice.fire;
+  }
+  return chart;
+}
+
+export function getOffensiveChartForVersionGroup(versionGroup) {
+  return getOffensiveChartForGeneration(getGameVersionGroup(versionGroup).generationNumber);
+}
