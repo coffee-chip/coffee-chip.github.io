a1f00b9e532f38b3129b50bfd08be6f680fe748f
coffee-chip <jlsieving@gmail.com>
2026-08-20 22:30:46 -0400

    Update PWA for game type chart cache

--- pokemon/service-worker.js
+++ pokemon/service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.08.20.2';
+const APP_VERSION = '2026.08.20.3';
 const CACHE_PREFIX = 'pokemon-type-trainer-shell-';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 const CORE_ASSETS = [
@@ -46,6 +46,7 @@ const CORE_ASSETS = [
   './js/data/mnemonics.js',
   './js/data/gameVersions.js',
   './js/data/gameSelection.js',
+  './js/data/typeChartRepository.js',
   './js/data/moveRepository.js',
   './js/data/starredMoveRepository.js',
   './js/data/pokemonRepository.js',
